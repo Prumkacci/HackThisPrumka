c9313e75a2a68995d3768cbb86e0fbc7348c20ba
Ondraaaseeek <ondronicek@gmail.com>
2021-07-14 09:31:28 +0200

    styly

--- WEB/Quest1/index.php
+++ WEB/Quest1/index.php
@@ -7,18 +7,34 @@
     <meta name='keywords' content='Quest'>
     <meta name='author' content='Ondřej Novotný'>
     <meta name='robots' content='all'>
-    <link rel="style" href="style.css">
+    <link rel="stylesheet" type="text/css" href="./style.css">
     <!-- <meta http-equiv='X-UA-Compatible' content='IE=edge'> -->
-    <link href='/favicon.png' rel='shortcut icon' type='image/png'>
+    <link rel="stylesheet" href="index.css?v=<?php echo time(); ?>">
+
   </head>
     <body>
-        <center>
-          <div>V tomto úkolu jde o úplný základ HTML. Hodně štěstí </div>
-            <a href = "https://www.w3schools.com/html/html_intro.asp" target="_blank"><b>Pro neznalé</b></a><br><br>
-            <b>Heslo:</b>
+        
+    <center>
+         <div class="nadpis">V tomto úkolu jde o úplný základ HTML. Hodně štěstí </div>
+          
+            
+         <span id="wrapper">
+
+
+	
+<a href="https://www.w3schools.com/html/html_intro.asp" target="_blank"  class="button ten">Pro neznalé.</a>
+	
+
+</span>   
+
+
+</div>
+            
+          <b>Heslo:</b>
             <?php
             $heslo1 = "frea4578";
-              echo "<br><form action=\"/HackThisPrumka/WEB/Quest1/index.php\" method=\"post\">
+             
+            echo "<br><form action=\"/HackThisPrumka/WEB/Quest1/index.php\" method=\"post\">
                 <input type=\"password\" name=\"password\" value = \"\"><br><br>
                 <input type=\"submit\" value=\"Odeslat\"></form>
                 <!-- Heslo je: " . $heslo1 . " -->";
@@ -26,9 +42,10 @@
             
             if ( isset($_POST["password"]) && $_POST["password"] == $heslo1)
             {
-              echo "<b>Gratuluji Dokončil jsi úkol 1.<b>";
+              echo "<h1>Gratuluji Dokončil jsi úkol 1.<h1>";
             }
                 ?>
+          
         </center>
     </body>
 </html>
