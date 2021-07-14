626204d332ba44dfb5379bba5e5955634d008ff9
Ondraaaseeek <ondronicek@gmail.com>
2021-07-14 09:39:34 +0200

    Quest6

---
+++ WEB/Quest6/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang='cs'>
+  <head>
+    <title>Quest No. 6</title>
+    <meta charset='utf-8'>
+    <meta name='description' content='úkol 6'>
+    <meta name='keywords' content='Quest'>
+    <meta name='author' content='Ondřej Novotný'>
+    <meta name='robots' content='all'>
+    <link rel="style" href="style.css">
+    <!-- <meta http-equiv='X-UA-Compatible' content='IE=edge'> -->
+    <link href='/favicon.png' rel='shortcut icon' type='image/png'>
+  </head>
+    <body>
+        <center>
+          <div>Bezpečný Jarda si název souboru ve kterém se heslo nachází zašifroval.</div>
+          <div>Jarda si ještě zkusil vytvořit script co mu zobrazí kalendář podle roku co se zadá.</div>
+            <?php
+              echo "<br><form action=\"/HackThisPrumka/WEB/Quest6/index.php\" method=\"post\">
+              <input type=\"text\" name=\"kalendar\" value = \"\"><br><br>
+              <input type=\"submit\" value=\"Odeslat\"></form>";
+
+              echo "<br><b>Heslo:</b><br><form action=\"/HackThisPrumka/WEB/Quest3/index.php\" method=\"post\">
+                <input type=\"password\" name=\"password\" value = \"\"><br><br>
+                <input type=\"submit\" value=\"Odeslat\"></form>";           
+            if ( isset($_POST["password"]) && $_POST["password"] == file_get_contents("hjgfvasafvdv1a5fgfdklgafasga4fg65d.txt"))
+            {
+              echo "<b>Gratuluji Dokončil jsi úkol 6.<b>";
+            }
+            
+            if( isset($_POST["kalendar"])){
+                    exec(cal, $vystup);
+                        print($vystup);
+                    
+            }
+                ?>
+        </center>
+    </body>
+</html>
