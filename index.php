7b6adfc87a9e9a0fcf010e0ef9849005a83c5193
Ondraaaseeek <ondronicek@gmail.com>
2021-07-16 22:46:12 +0200

    Registace Main Page

--- index.php
+++ index.php
@@ -17,7 +17,47 @@
             session_start();
             if(!isset($_SESSION["loginMenu"])){
                 $_SESSION["loginMenu"] = 1;
+               
             }
+
+            $servername = "localhost";
+            $username = "ondrasek";
+            $password = "Ondra2580,";
+            $dbname = "HackThisPrumka";
+
+            $conn = new mysqli($servername, $username, $password, $dbname);
+
+            if($conn->connection_error){
+                die("Připojení selhalo: " .  $conn->connect_error);
+            }
+
+            
+            if(isset($_POST["Registrovat"])){
+                $jmeno  =   $_POST["username"];
+                $heslo  =   $_POST["password"];
+                $heslo2 =   $_POST["password2"];
+                $sql = "SELECT name FROM Ucty WHERE name='". $jmeno ."'";
+                $result = $conn->query($sql);
+
+                if($result->num_rows > 0){
+                    echo "Uživatel s tímto jménem je již zaregistrován.";
+                }
+                else{
+                    if($heslo == $heslo2){
+                        $sql = "INSERT INTO Ucty (name, password) VALUES ($jmeno, $heslo)";
+                        if($conn->query($sql) == TRUE){
+                            echo "Uživatel vytvořen.";
+                        }
+                    }
+                    else{
+                        echo "Hesla se neshodují.";
+                    }
+                }
+                
+            }
+
+
+
         function login()
         {
             echo"<h3>Přihlášení</h3><br/>";
@@ -27,7 +67,7 @@
                 <input type=\"text\" name=\"username\" value = \"\"><br/>
                 <div>Heslo:</div>
                 <input type=\"password\" name=\"password\" value = \"\"><br/>
-                <input type=\"submit\" value=\"Přihlasit se\">
+                <input type=\"submit\" value=\"Přihlasit se\" name='Prihlasit'>
             </form>";
             echo"
                 <br/>
@@ -47,7 +87,7 @@
                 <input type=\"password\" name=\"password\" value = \"\"><br/>
                 <div>Znovu heslo:</div>
                 <input type=\"password\" name=\"password2\" value = \"\"><br/>
-                <input type=\"submit\" value=\"Zaregistrovat se\">
+                <input type=\"submit\" value=\"Zaregistrovat se\" name='Registrovat'>
             </form>";
             echo"
                 <br/>
@@ -69,7 +109,10 @@
         else if($_SESSION["loginMenu"] == 2){
             register();
         }
+        
+        function showMenu(){
 
+        }
           ?>
           
         
