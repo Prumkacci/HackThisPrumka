505f67939b3dbb044376a792734a6ae61a330d9b
Ondraaaseeek <ondronicek@gmail.com>
2021-08-28 14:47:47 +0200

    Styly

--- Quest3/index.php
+++ Quest3/index.php
@@ -7,21 +7,40 @@
     <meta name='keywords' content='Quest'>
     <meta name='author' content='Ondřej Novotný'>
     <meta name='robots' content='all'>
-    <link rel="style" href="style.css">
+    <link rel="stylesheet" href="index.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="style.css">
     <!-- <meta http-equiv='X-UA-Compatible' content='IE=edge'> -->
     <link href='/favicon.png' rel='shortcut icon' type='image/png'>
   </head>
     <body>
-        <center>
-          <div>Bezpečný Jarda si tuto stránku zabezpečil pomocí hesla v souboru. Tentokrát ho dokonce nahrál.</div>
-            <b>Heslo:</b>
-            <?php
-              echo "<br><form action=\"/HackThisPrumka/Quest3/index.php\" method=\"post\">
-                <input type=\"password\" name=\"password\" value = \"\"><br><br>
-                <input type=\"submit\" value=\"Odeslat\"></form>";           
+      <?php
+    ob_start(); echo "<center>
+          <div class='nadpis'>Bezpečný Jarda si tuto stránku zabezpečil pomocí hesla v souboru. Tentokrát ho dokonce nahrál.</div>
+          </center> <div class='psani'> <h1>PŘIHLÁŠENÍ</h1> </div>
+            
+                <br><form action=\"/HackThisPrumka/Quest3/index.php\" method=\"post\">
+                <div class ='jmeno'> <label for='jmeno'> Jméno:</label> <br> <input type=\"text\" name=\"jmeno\" value = \"Admin\" placeholder=\"Jméno\"><br><br> </div class='jmeno'> 
+            
+                <div class ='heslo'> <label for='password'> Heslo:</label> <br> <input type=\"password\" name=\"password\" value = \"\" placeholder=\"Heslo\"><br><br> </div class='heslo'>
+                <div class='tlacitko'> <center> <input type=\"submit\" value=\"Odeslat\"> </center> </form> </div class ='tlacitko'>";  
             if ( isset($_POST["password"]) && $_POST["password"] == file_get_contents("heslo"))
             {
-              echo "<b>Gratuluji Dokončil jsi úkol 3.<b>";
+              ob_end_clean(); echo
+              "<div class='confetti'>
+              <div class='confetti-piece'></div>
+              <div class='confetti-piece'></div>
+              <div class='confetti-piece'></div>
+              <div class='confetti-piece'></div>
+              <div class='confetti-piece'></div>
+              <div class='confetti-piece'></div>
+              <div class='confetti-piece'></div>
+              <div class='confetti-piece'></div>
+              <div class='confetti-piece'></div>
+              <div class='confetti-piece'></div>
+              <div class='confetti-piece'></div>
+              <div class='confetti-piece'></div>
+              <div class='confetti-piece'></div>
+          </div> <h2> Gratuluji, dokončil jsi úkol číslo 3!</h2>";
             }
                 ?>
         </center>
--- Quest5/index.php
+++ Quest5/index.php
@@ -7,44 +7,64 @@
     <meta name='keywords' content='Quest'>
     <meta name='author' content='Ondřej Novotný'>
     <meta name='robots' content='all'>
-    <link rel="style" href="style.css">
+    <link rel="stylesheet" href="index.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="style.css">
     <!-- <meta http-equiv='X-UA-Compatible' content='IE=edge'> -->
     <link href='/favicon.png' rel='shortcut icon' type='image/png'>
   </head>
     <body>
-        <center>
-        <?php
-        $heslo5 = "rafe485sdc";
+    <?php
+    $heslo5 = "rafe485sdc";
+    function sifra($vstup){
+      $vysledek = "";
+      $vstup = str_split($vstup);
+      for($i = 0;$i != count($vstup); $i++){
+          $vysledek .= chr(ord($vstup[$i]) + ($i + 1));
 
-            function sifra($vstup){
-                $vysledek = "";
-                $vstup = str_split($vstup);
-                for($i = 0;$i != count($vstup); $i++){
-                    $vysledek .= chr(ord($vstup[$i]) + ($i + 1));
+      }
+      return $vysledek;
+  }
+ 
 
-                }
-                return $vysledek;
-            }
+    ob_start(); echo "<center> 
 
-          echo "<div>Bezpečný Jarda si heslo zašifroval. bohužel veřejně dostupným šifrovačem. Podařilo se ti dostat heslo v zašifrované podobě " . sifra($heslo5) . "</div>";
-            
+           <div class='nadpis'> <h1>Bezpečný Jarda si heslo zašifroval. bohužel veřejně dostupným šifrovačem. Podařilo se ti dostat heslo v zašifrované podobě <br/><u>" . sifra($heslo5) . "</u></h1> </div>
+           
 
-            echo "<br/><b>Šifrovací engine<b><br><form action=\"/HackThisPrumka/Quest5/index.php\" method=\"post\">
-                <input type=\"text\" name=\"text\"><br><br>
-                <input type=\"submit\" value=\"Odeslat\"></form>";
+                <div class='psani'> <h1>Šifrovací engine</h1> </div> 
+                <form action=\"/HackThisPrumka/Quest5/index.php\" method=\"post\">
+                <div class='jmeno'> <input type=\"text\" name=\"text\"><br><br> </div>
+                <div class='tlacitko'> <input type=\"submit\" value=\"Zašifrovat\"></form> </div>"; 
 
                 if ( isset($_POST["text"]))
-              {
-              echo "<br>Tvoje šifra je " . sifra($_POST["text"]);
-              }
+                {
+                  echo "<div class='sifravys'> <br>Tvoje šifra je  " . sifra($_POST["text"]). "</div>";
+                }
 
-              echo "<br><br><b>Heslo:</b> <br><form action=\"/HackThisPrumka/Quest5/index.php\" method=\"post\">
-                <input type=\"password\" name=\"password\" value = \"\"><br><br>
-                <input type=\"submit\" value=\"Odeslat\"></form>";           
+                echo "<div class='psani'> <h1>PŘIHLÁŠENÍ</h1> </div><br><form action=\"/HackThisPrumka/Quest5/index.php\" method=\"post\">
+                
+                <div class ='jmeno'> <label for='jmeno'> Jméno:</label> <br> <input type=\"text\" name=\"jmeno\" value = \"Admin\" placeholder=\"Jméno\"><br><br> </div class='jmeno'> 
+                <div class ='heslo'> <label for='password'> Heslo:</label> <br> <input type=\"password\" name=\"password\" value = \"\" placeholder=\"Heslo\"><br><br> </div class='heslo'>
+                <div class='tlacitko'> <center> <input type=\"submit\" value=\"Odeslat\"> </center> </form> </div class ='tlacitko'>";         
             if ( isset($_POST["password"]) && $_POST["password"] == $heslo5)
             {
-              echo "<b>Gratuluji Dokončil jsi úkol 5.<b>";
+              ob_end_clean(); echo"<div class='confetti'>
+              <div class='confetti-piece'></div>
+              <div class='confetti-piece'></div>
+              <div class='confetti-piece'></div>
+              <div class='confetti-piece'></div>
+              <div class='confetti-piece'></div>
+              <div class='confetti-piece'></div>
+              <div class='confetti-piece'></div>
+              <div class='confetti-piece'></div>
+              <div class='confetti-piece'></div>
+              <div class='confetti-piece'></div>
+              <div class='confetti-piece'></div>
+              <div class='confetti-piece'></div>
+              <div class='confetti-piece'></div>
+          </div> <h2> Gratuluji, dokončil jsi úkol číslo 5!</h2>";
             }
+        
         ?>
         </center>
     </body>
