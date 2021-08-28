0b9187ffd1810161f39e510e8f532c4ab93b7881
Ondraaaseeek <ondronicek@gmail.com>
2021-08-28 15:52:41 +0200

    Quest6 lang fix

--- Quest6/kalendar.php
+++ Quest6/kalendar.php
@@ -1,7 +1,10 @@
+<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
 <?php
+    header('Content-type: text/html; charset=utf-8');
     $vystup = "";
+    putenv('LANG=cs_CZ.UTF-8');
     exec('cal '. $_POST["kalendar"], $vystup);
-    echo    "<pre>";
+    echo "<pre>";
     echo implode("<br>",$vystup);
     echo "</pre>";
 
