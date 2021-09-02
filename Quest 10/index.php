04db1d13be51501c21f4ae6e0cc1db3cb48608e6
Ondraaaseeek <ondronicek@gmail.com>
2021-09-02 21:53:04 +0200

    Quest 10 begin

---
+++ Quest 10/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang='cs'>
+  <head>
+    <title>Quest No. 10</title>
+    <meta charset='utf-8'>
+    <meta name='description' content='úkol 10'>
+    <meta name='keywords' content='Quest'>
+    <meta name='author' content='Ondřej Novotný'>
+    <meta name='robots' content='all'>
+    <link rel="style" href="style.css">
+    <!-- <meta http-equiv='X-UA-Compatible' content='IE=edge'> -->
+    <link href='/favicon.png' rel='shortcut icon' type='image/png'>
+  </head>
+    <body>
+        <?php
+            $cislo = rand(1, 5);
+            if($cislo == 1){
+                echo "Ta naše průmka je úžasná.";
+            }
+            if($cislo == 2){
+                echo "Ten web té naší průmky je krásnej.";
+            }
+            if($cislo == 3){
+                echo "Ještě že má ta naše průmka tak skvělé vybavení.";
+            }
+            if($cislo == 4){
+                echo "Kéž by každá škola byla jako naše průmka.";
+            }
+            if($cislo == 5){
+                echo "Průmka je život.";
+            }
+
+        ?>
+    </body>
+</html>
